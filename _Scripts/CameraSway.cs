4e8d7225e31ae55e6e258ace76b158dc72280d1d
Viz <95000221+VizzottoNCF@users.noreply.github.com>
2025-10-07 16:12:23 -0300

    added dotween

---
+++ _Scripts/CameraSway.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CameraSway : MonoBehaviour
+{
+    private bool swayingRight = true;
+    private float limitX = 0.3f;
+    private float timeToSway = 2f;
+    private void Start()
+    {
+        
+    }
+}
